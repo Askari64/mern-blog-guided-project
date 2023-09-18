5f6a596808173c90730a5985ca06fa8598f6d473
Askari <askarir64@gmail.com>
2023-09-18 23:00:09 +0530

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,46 @@
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900 ">Hello, Welcome to my Blog</h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, nostrum!
+        Laboriosam quisquam accusamus rem unde nemo eveniet, exercitationem
+        sequi id cupiditate cum, beatae aspernatur magni quidem cumque labore
+        similique voluptatum minima sapiente inventore nam in quos officiis!
+        Cumque facilis aperiam consequatur delectus natus, odio non incidunt
+        explicabo nulla repudiandae iste.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, nostrum!
+        Laboriosam quisquam accusamus rem unde nemo eveniet, exercitationem
+        sequi id cupiditate cum, beatae aspernatur magni quidem cumque labore
+        similique voluptatum minima sapiente inventore nam in quos officiis!
+        Cumque facilis aperiam consequatur delectus natus, odio non incidunt
+        explicabo nulla repudiandae iste.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, nostrum!
+        Laboriosam quisquam accusamus rem unde nemo eveniet, exercitationem
+        sequi id cupiditate cum, beatae aspernatur magni quidem cumque labore
+        similique voluptatum minima sapiente inventore nam in quos officiis!
+        Cumque facilis aperiam consequatur delectus natus, odio non incidunt
+        explicabo nulla repudiandae iste.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In beatae quia
+        illo voluptas reprehenderit ea aspernatur quos. Porro illo mollitia odit
+        deserunt minus error ab, cupiditate officiis natus, distinctio fugiat
+        assumenda, officia nihil nobis. Fuga quibusdam facilis hic minima quos
+        rerum ducimus qui mollitia culpa saepe possimus modi temporibus alias,
+        amet nisi illo officia! Esse, molestias ad exercitationem cum nulla
+        debitis doloribus dolorem aliquam distinctio eos inventore eaque
+        deleniti, culpa, aliquid quisquam. Doloribus atque consequuntur quis
+        blanditiis eum laborum accusantium rerum vel beatae id, quaerat mollitia
+        officia similique repudiandae, sit saepe soluta incidunt culpa
+        aspernatur! Quos ab laboriosam eligendi porro?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
